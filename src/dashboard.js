021bbac57f92df4185926a4032c182df6a145a8d
kaviyarashup <kaviyarashupkiaq@gmail.com>
2026-03-09 10:29:51 +0530

    first commit

---
+++ src/dashboard.js
@@ -0,0 +1,28 @@
+function dashboard(){
+
+return `
+
+<div class="cards">
+
+<div class="card">
+<h3>Servers</h3>
+<p>10</p>
+</div>
+
+<div class="card">
+<h3>Deployments</h3>
+<p>25</p>
+</div>
+
+<div class="card">
+<h3>Users</h3>
+<p>150</p>
+</div>
+
+</div>
+
+`;
+
+}
+
+module.exports = dashboard;
---
+++ src/navbar.js
@@ -0,0 +1,11 @@
+function navbar(){
+
+return `
+<div class="navbar">
+<h2>DevOps Dashboard</h2>
+</div>
+`;
+
+}
+
+module.exports = navbar;
